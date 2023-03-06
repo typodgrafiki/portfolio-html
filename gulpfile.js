06e8231b29ac00db967d85a7d7b6ba218953f2db
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-03-06 17:02:15 +0100

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-function defaultTask(cb) {
-    cb();
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+function myTask(cb) {
+
+    
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+            server: {
+                baseDir: "./"
+            }
+        });
+        
+        gulp.watch("*.html").on("change", reload);
+        
+    });
 }
+
+
   
-exports.default = defaultTask
+exports.default = myTask
